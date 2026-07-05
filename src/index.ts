07a10dd5ecb14aff01ced34c742955b25b9ec060
Duc Nguyen <minhduc140583@gmail.com>
2026-07-05 22:19:54 +0700

    Format code

--- src/index.ts
+++ src/index.ts
@@ -1,104 +1,104 @@
-import ProcessEnv = NodeJS.ProcessEnv;
+import ProcessEnv = NodeJS.ProcessEnv
 
 export function merge<T>(cfg: T, env: ProcessEnv, extEnv?: any, envName?: string): T {
   if (!extEnv || !envName || envName.length === 0) {
-    return mergeEnv(cfg, env);
+    return mergeEnv(cfg, env)
   } else {
-    const x = extEnv[envName];
+    const x = extEnv[envName]
     if (x) {
-      const c2 = mergeEnvironments(cfg, extEnv[envName]);
-      return mergeEnv(c2, env);
+      const c2 = mergeEnvironments(cfg, extEnv[envName])
+      return mergeEnv(c2, env)
     } else {
-      return mergeEnv(cfg, env);
+      return mergeEnv(cfg, env)
     }
   }
 }
 export function mergeEnvironments<T>(cfg: T, cfgEnv?: any): T {
   if (!cfgEnv) {
-    return cfg;
+    return cfg
   }
-  const conf: any = cfg;
-  const keys = Object.keys(cfgEnv);
+  const conf: any = cfg
+  const keys = Object.keys(cfgEnv)
   for (const key of keys) {
-    const o2 = cfgEnv[key];
+    const o2 = cfgEnv[key]
     switch (typeof o2) {
-      case 'object':
+      case "object":
         if (Array.isArray(o2)) {
-          conf[key] = o2;
+          conf[key] = o2
         } else {
-          const o1 = conf[key];
-          if (o1 && typeof o1 === 'object' && !Array.isArray(o1)) {
-            mergeEnvironments(o1, o2);
+          const o1 = conf[key]
+          if (o1 && typeof o1 === "object" && !Array.isArray(o1)) {
+            mergeEnvironments(o1, o2)
           }
         }
-        break;
+        break
       default:
         if (o2 !== conf[key]) {
-          conf[key] = o2;
+          conf[key] = o2
         }
-        break;
+        break
     }
   }
-  return conf;
+  return conf
 }
 export function mergeEnv<T>(cfg: T, env: ProcessEnv): T {
-  return mergeWithPath({ ...cfg }, env, undefined);
+  return mergeWithPath({ ...cfg }, env, undefined)
 }
 export function mergeWithPath<T>(cfg: T, env: ProcessEnv, parentPath?: string): T {
-  const conf: any = cfg;
-  const keys = Object.keys(conf);
+  const conf: any = cfg
+  const keys = Object.keys(conf)
   for (const key of keys) {
-    const envKey = buildFullPathEnv(key, parentPath);
-    const envValue = env[envKey];
+    const envKey = buildFullPathEnv(key, parentPath)
+    const envValue = env[envKey]
     switch (typeof conf[key]) {
-      case 'string':
+      case "string":
         if (envValue && envValue.length > 0) {
           // console.log('Override by environment parameter: ' + envKey);
-          conf[key] = envValue;
+          conf[key] = envValue
         }
-        break;
-      case 'object':
+        break
+      case "object":
         if (Array.isArray(conf[key])) {
           try {
             if (envValue) {
-              const newArray = JSON.parse(envValue);
-              if (typeof newArray === 'object' && Array.isArray(newArray)) {
-                conf[key] = newArray;
+              const newArray = JSON.parse(envValue)
+              if (typeof newArray === "object" && Array.isArray(newArray)) {
+                conf[key] = newArray
               }
             }
           } catch (e) {
-            console.log("Can't parse value of " + envKey + ' env', e);
+            console.log("Can't parse value of " + envKey + " env", e)
           }
         } else if (conf[key] !== null) {
-          conf[key] = mergeWithPath(conf[key], env, envKey);
+          conf[key] = mergeWithPath(conf[key], env, envKey)
         }
-        break;
-      case 'number':
+        break
+      case "number":
         if (envValue && envValue.length > 0 && !isNaN(envValue)) {
-          conf[key] = Number(envValue);
+          conf[key] = Number(envValue)
         }
-        break;
-      case 'boolean':
+        break
+      case "boolean":
         if (envValue) {
-          const nv = env[envKey] === 'true';
+          const nv = env[envKey] === "true"
           if (nv !== conf[key]) {
-            conf[key] = nv;
+            conf[key] = nv
           }
         }
-        break;
+        break
       default:
-        break;
+        break
     }
   }
-  return conf;
+  return conf
 }
 function buildFullPathEnv(key: string, parentPath?: string): string {
   if (isEmpty(parentPath)) {
-    return key.toUpperCase();
+    return key.toUpperCase()
   } else {
-    return parentPath + '_' + key.toUpperCase();
+    return parentPath + "_" + key.toUpperCase()
   }
 }
 function isEmpty(s?: string): boolean {
-  return !s || s === '';
+  return !s || s === ""
 }
